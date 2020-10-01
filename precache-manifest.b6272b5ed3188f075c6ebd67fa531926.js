7280edefa7b3f4301d4525f89f3e2ffe68d4564b
Hjinna <1326687396@qq.com>
2020-10-01 21:05:55 +0800

    react 1

---
+++ precache-manifest.b6272b5ed3188f075c6ebd67fa531926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cda71b1c40eb19c11a52b32fe6c5740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32bd928cdda712f6696",
+    "url": "/static/js/2.1413a434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1413a434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d96ec9188884f6ed847",
+    "url": "/static/js/main.c6abaf1a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
